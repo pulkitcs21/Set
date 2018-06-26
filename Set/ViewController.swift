a711d4f1c29aaf3c2edafacb600bd9267e106884
Pulkit <pulkit.agarwal@sjsu.edu>
2018-06-26 14:43:27 -0700

    Intialize

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Intialize
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
